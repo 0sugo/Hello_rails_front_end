f30067b001a4b764bd88b4696712973c67c7c801
0sugo <josugo26@gmail.com>
2023-08-16 00:21:01 +0300

    Configure redux

---
+++ src/redux/greetings/greetingSlice.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  greetings: [],
+};
+
+export const fetchGreetings = createAsyncThunk(
+  'greetings/fetchGreetings',
+  async () => {
+    const response = await fetch('http://localhost:3000/api/v1/random_greeting');
+    const data = await response.json();
+    console.log(data)
+    return data;
+  },
+);
+
+const greetingSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchGreetings.fulfilled, (state, action) => {
+        state.greetings = action.payload;
+      });
+  },
+});
+
+export default greetingSlice.reducer;
