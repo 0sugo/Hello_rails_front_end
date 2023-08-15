2a9ba409ac73cc52115caf6fc74e383c044a94c5
0sugo <josugo26@gmail.com>
2023-08-16 01:06:38 +0300

    Add and fix linters

--- src/redux/greetings/greetingSlice.js
+++ src/redux/greetings/greetingSlice.js
@@ -9,7 +9,6 @@ export const fetchGreetings = createAsyncThunk(
   async () => {
     const response = await fetch('http://localhost:3000/api/v1/random_greeting');
     const data = await response.json();
-    console.log(data)
     return data;
   },
 );
